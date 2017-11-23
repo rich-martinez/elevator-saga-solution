fc8df9b2bec4b65e63613b57591fbccfef387d5a
Rich Martinez <richmartinez@quickenloans.com>
2017-11-22 19:57:33 -0500

    Add basic for navigating to floors

---
+++ init.js
@@ -0,0 +1,40 @@
+{
+    init: function(elevators, floors) {
+        elevators.forEach(function (elevator) {
+            //
+            // for each elevator add listeners for every floor
+            //
+            floors.forEach(function (floor) {
+                var floorNumber = floor.floorNum();
+
+                floor.on('up_button_pressed', function() {
+                    console.log(`up button pressed on floor ${floorNumber}`);
+                    elevator.goToFloor(floorNumber);
+                });
+
+                floor.on('down_button_pressed', function() {
+                    console.log(`down button pressed on floor ${floorNumber}`);
+                    elevator.goToFloor(floorNumber);
+                });
+            });
+
+            elevator.on('floor_button_pressed', function(floorPressed) {
+                console.log(`floor button pressed for floor ${floorPressed}`);
+                elevator.goToFloor(floorPressed);
+            });
+
+            elevator.on('stopped_at_floor', function (currentFloorNumber) {
+                console.log(`stopped at floor number ${currentFloorNumber}`);
+            });
+
+            elevator.on("passing_floor", function(currentFloorNumber, direction) {
+                const pressedFloors = elevator.getPressedFloors();
+                const destinationQueue = elevator.destinationQueue;
+                console.log(`passing floor number ${currentFloorNumber} and the current direction is ${direction}`);
+            });
+        });
+    },
+    update: function(dt, elevators, floors) {
+        // We normally don't need to do anything here
+    }
+}
